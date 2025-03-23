b6574a48586f6503a2d08f4d2bedd2aa32637db2
nsyafantrin <nsyafantrin@gmail.com>
2025-03-23 15:37:24 +0700

    starting learning JS
    variables and operators

---
+++ javascript.js
@@ -0,0 +1,41 @@
+//practice 1
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+//practice 2
+let age = 11;
+console.log(age);
+
+age = 40; //reasigned
+
+console.log(age);
+
+//practice 3
+const pi = 20;
+// pi = 10; // can't reassigned
+
+console.log(pi);
+
+// ASSIGNMENT
+console.log((4+6+9)/77);
+
+let a = 10;
+a = 12;
+console.log(a);
+
+// a = 12;
+// console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+
+// ASSIGNMENT 2
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+
+console.log(percentage);
