d7f17aba981c32e905c9909a10037e1b210de4d1
nsyafantrin <nsyafantrin@gmail.com>
2025-03-25 22:47:28 +0700

    learning JS
    from : javascript.info
    
    finish data type,
    continue of interaction 2.6

--- javascript.js
+++ javascript.js
@@ -39,3 +39,53 @@ const actual = max - 13;
 const percentage = actual / max;
 
 console.log(percentage);
+
+alert('hello world!');
+
+// DATA TYPE : NUMBER
+let naaa = 123;
+console.log(naaa);
+
+// DATA TYPE : BIGINT
+console.log(9007199254740991 + 1); // 9007199254740992
+console.log(9007199254740991 + 2); // 9007199254740992
+
+const bigInt = 1234567890123456789012345678901234567890n;
+console.log(bigInt);
+
+
+// DATA TYPE : STRING
+let message = "dup";
+
+alert (message);
+
+let namess = "John";
+let admin = namess;
+alert(admin);
+
+let namesss = "doidoi";
+alert (`hei, ${namesss}`);
+
+
+// DATA TYPE : BOOLEAN (LOGICAL TYPE)
+let namefeild = true;
+let nameage = false;
+
+alert(namefeild);
+alert(nameage);
+
+
+// DATA TYPE : NULL
+let aging = null;
+
+console.log(aging);
+
+
+// DATA TYPE : Undefined Value
+let agge;
+console.log(agge);
+
+
+// DATA TYPE : the typeoff
+
+typeof 0;
