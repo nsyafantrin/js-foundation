ef551ee51e497641e0b009afe1f991a40fa01503
nsyafantrin <nsyafantrin@gmail.com>
2025-03-27 16:28:40 +0700

    alert, prompt, confirm
    learning alert, prompt, confirm.

---
+++ interaction.js
@@ -0,0 +1,11 @@
+// ALERT
+alert("hello");
+
+// PROMPT
+let age = prompt("how old are you ?", 27);
+// let age = 20;
+alert(`you are ${age} years old!`);
+
+// CONFIRM
+result = confirm("Are you the boss?");
+alert(result);
