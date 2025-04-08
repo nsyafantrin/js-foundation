7b3cc936031f2b19d053a06482bf18b31c8a026d
nsyafantrin <nsyafantrin@gmail.com>
2025-04-09 05:25:01 +0700

    Operators >> continue numeric conversion, unary +

--- operators.js
+++ operators.js
@@ -4,3 +4,45 @@ console.log("TERMS: UNARY, BINARY, OPERAND");
 let x = 1;
 x = -x;
 console.log(x);
+
+let y = 3;
+console.log( y - x);
+
+// MATHs
+console.log("MATHs");
+console.log ( y + x);
+console.log ( y - x);
+console.log ( y * x);
+console.log ( y / x);
+
+// Remainder
+console.log("Remainder");
+console.log( 5 % 2);
+console.log( 8 % 5);
+console.log( 8 % 4);
+
+// Exponentiation
+console.log("Exponentiation");
+console.log( 2 ** 2);
+console.log( 2 ** 3);
+console.log( 2 ** 4);
+
+console.log( "Square root is an exponentiation by 1/2");
+console.log(4 ** (1/2));
+console.log(8 ** (1/3));
+console.log(16 ** (1/4));
+
+// String concatenation with binary +
+console.log("String concatenation with binary +");
+let s = "- my" + "string";
+console.log(s);
+
+console.log('1' + 2 );
+console.log(2 + '1');
+
+console.log( 2 + 2 + '1');
+console.log( '1' + 2 + 2);
+
+console.log("example only operator + ")
+console.log(6 - '2');
+console.log('6' / '2');
