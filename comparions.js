9d67d7c178e7ae7046e4ecec1499667476abc620
nsyafantrin <nsyafantrin@gmail.com>
2025-04-15 06:40:33 +0700

    continue comparison of different types

---
+++ comparions.js
@@ -0,0 +1,18 @@
+console.log("boolean is the result")
+console.log( 2 > 1); 
+console.log( 2 == 1);
+console.log( 2 != 1);
+
+let result  = 5 > 4;
+console.log(result);
+
+console.log("Strigs comparisons");
+console.log('z' > 'a');
+console.log('Glow' > 'Glee');
+console.log('Bee' > 'Be');
+
+console.log("Comparisons of different types");
+console.log( '2' > 1); // true, string is converted to number
+console.log( '01' == 1); // true, string is converted to number
+console.log( true == 1); // true, boolean is converted to number
+console.log( false == 0); // true, boolean is converted to number
