c615d32b3cf6deee78b9073bae228c034a1c835e
nsyafantrin <nsyafantrin@gmail.com>
2025-04-18 00:53:22 +0700

    finish ifelse

--- conditional.js
+++ conditional.js
@@ -43,16 +43,65 @@
 // }
 
 // CONDITIONAL OPERATORS
-let accessAllowed;
-let age = prompt('how old are you?');
-
-if (age > 18 ) {
-    accessAllowed = true;
-} else {
-    accessAllowed = false;
-}
-
+// let accessAllowed;
 // let age = prompt('how old are you?');
-// let accessAllowed = (age > 18) ? true : false;
 
-console.log(accessAllowed);
+// if (age > 18 ) {
+//     accessAllowed = true;
+// } else {
+//     accessAllowed = false;
+// }
+
+// // let age = prompt('how old are you?');
+// // let accessAllowed = (age > 18) ? true : false;
+
+// console.log(accessAllowed);
+
+// MULTIPLE ?
+// let age = prompt('age?', 18);
+// if (age < 3) {
+//     message = 'Hi, baby!';
+// } else if (age < 18) {
+//     message = 'Hello!';
+// } else if (age < 100) {
+//     message = 'Greetings!';
+// } else {
+//     message = 'What an unusual age!';
+// }
+// alert(message);
+
+// // NON TRADITIONAL USE OF ?
+// let company = prompt('which company created javascript?' , '');
+
+// (company == 'Netscape') ?
+// alert('Right!') : alert('Wrong!');
+
+// TASK
+// // 1. if (a string with zero)
+// if ("0") {
+//     alert( 'Hello' );
+// }
+
+// // 2. The name of javascript
+// let name = prompt ("what is the 'official' name of javscript?");
+// if (name == 'ECMAScript') {
+//     alert('Correct!');
+// }
+// else {
+//     alert('You don\'t know? ECMAScript!');
+// }
+
+// 3. Rewrite 'if' into '?'
+// let result
+// (a+b<4) ? 'Below' : 'Over';
+
+// 4. Rewrite 'if' into '?'
+
+let login = prompt ('who are you?');
+
+let message = (login == 'Employee') ? 'Hello' :
+    (login == 'Director') ? 'Greetings':
+        (login == '') ? 'No login':
+            'Unknown user';
+
+alert(message);
