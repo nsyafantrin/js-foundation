0ce7f0ba42958d6b43711be7211ef7ac037cb8e1
nsyafantrin <nsyafantrin@gmail.com>
2025-05-12 00:58:43 +0700

    practice string javascript

---
+++ strings.js
@@ -0,0 +1,22 @@
+// Learning source: https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Scripting/Strings#declaring_strings
+
+
+console.log("DECLARING STRINGS");
+const string = "The revolution will not be televised."
+
+console.log(string);
+
+const badString = string;
+console.log(badString);
+
+const dooo = string;
+console.log(dooo);
+
+console.log("SINGLE QUOTES, DOUBLE QUOTES, AND BACKTICKS");
+const single = 'Single Quotes';
+const double = "Double Quotes";
+const backticks = `Backtick`;
+
+console.log(single);
+console.log(double);
+console.log(backticks);
